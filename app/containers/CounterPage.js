b54f6a86257e524f955e0377e13e63d67a178815
Gabriel Löff <you@example.com>
2018-08-28 18:24:39 -0300

    Adicionando Echarts e Grafico exemplo

--- app/containers/CounterPage.js
+++ app/containers/CounterPage.js
@@ -1,19 +1,6 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import Counter from '../components/Counter';
-import * as CounterActions from '../actions/counter';
+import MyChart from '../components/MyChart';
 
-function mapStateToProps(state) {
-  return {
-    counter: state.counter
-  };
-}
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(CounterActions, dispatch);
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Counter);
+export default connect()(MyChart);
