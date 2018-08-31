ecd65a0bbb6b0256e9f6a0ecaee27d8c49ad6aca
leonardo <leonardovizzotto14@gmail.com>
2018-08-31 19:55:56 -0300

    componente upload csv

---
+++ app/components/UploadCsv/UploadCsv.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Dropzone from 'react-dropzone';
+
+type Props = {};
+
+export default class UploadCsv extends React.Component {
+  onDrop(acceptedFiles, rejectedFiles) {
+    const { rejectedFunction, acceptedFunction } = this.props;
+
+    if (acceptedFunction) {
+      acceptedFunction(acceptedFiles);
+    }
+    if (rejectedFunction) {
+      rejectedFunction(rejectedFiles);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Dropzone
+          accept=".csv"
+          onDrop={(accepted, rejected) => this.onDrop(accepted, rejected)}
+        >
+          <span>Arraste um arquivo csv aqui.</span>
+        </Dropzone>
+      </div>
+    );
+  }
+}
