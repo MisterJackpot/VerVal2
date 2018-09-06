28c6dbffc4d35c5bf059de4aa337923da0446254
johnPetter <joao.pedrolaureano@hotmail.com>
2018-09-06 14:20:17 -0300

    Método ONCHANGE funcionando

--- app/containers/Home/HomeContainer.js
+++ app/containers/Home/HomeContainer.js
@@ -7,19 +7,16 @@ import ImageWrapperComponent from '../../components/ImageWrapperComponent/ImageW
 import LoginInputComponent from '../../components/LoginInputComponent/LoginInputComponent';
 import UploadCsv from '../../components/UploadCsv/UploadCsv';
 import NavigateButtonComponent from '../../components/NavigateButtonComponent/NavigateButtonComponent';
-<<<<<<< HEAD
 import Senha from '../../utils/DB/DAO/Senha';
-=======
 import InputComponent from '../../components/LoginInputComponent/InputComponent';
->>>>>>> 18a9df7e465b96e6d34c9cea630f94b9a39ebbe7
 
-type Props = {}; 
+type Props = {};
 
 export default class HomeContainer extends Component<Props> {
 
   constructor(props) {
     super(props);
-    
+
     this.validarSenha = this.validarSenha.bind(this);
     this.state = {
       password: '',
@@ -27,20 +24,23 @@ export default class HomeContainer extends Component<Props> {
   }
 
   validarSenha = () => {
-    let teste = 'aa';
     Senha.getSenha(function(result){
       console.log(this);
       if(this.state.password == result) console.log("true");
     });
   }
 
-  onChange = event => {
+  onChange = (event) => {
+    this.setState({password: event.target.value});
+    console.log(event.target.value);
     //const { name, value } = event.target;
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
+  //  const target = event.target;
+  //  const name = target.name;
+    //const value = target.value;
     //console.log(event);
-    this.setState({ [name]: value });
+  //  this.setState({ [name]: value });
+//  this.setState({password : event.target.value});
+//    console.log(this.password);
   };
 
   render() {
@@ -48,11 +48,10 @@ export default class HomeContainer extends Component<Props> {
     return (
         <div className={styles.container} align="center" data-tid="container">
               <ImageWrapperComponent path={'./Assets/target.png'}/>
-              <InputComponent/> 
+              <InputComponent onChange={this.onChange}/>
               <NavigateButtonComponent text="Entrar" navigate={routes.COUNTER} />
         </div>
     );
-  } 
-
+  }
   props: Props;
 }
