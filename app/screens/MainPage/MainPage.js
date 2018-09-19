87cab7f83b29c772f651f0c07a0ce33365bd34ae
guilherme.piccoli <guilherme.piccoli@acad.pucrs.br>
2018-09-19 18:56:07 -0300

    Tela principal (grafico + lista) - bugada

--- app/screens/MainPage/MainPage.js
+++ app/screens/MainPage/MainPage.js
@@ -4,13 +4,17 @@ import CounterContainer from '../../containers/Counter/CounterContainer';
 import Chart3D from '../../components/MyCharts/Chart3D';
 import Chart2D from '../../components/MyCharts/Chart2D';
 import MyChartContainer from '../../containers/MyEchartContainer/MyEchartContainer'
-
+import ListContainer from '../../containers/ListaAmostras/ListaAmostrasContainer'
 type Props = {};
 
 export default class MainPage extends Component<Props> {
   props: Props;
 
   render() {
-    return <MyChartContainer/>;
-  }
+    return (
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+    <MyChartContainer/>
+    <ListContainer/>
+      </div>
+  )}
 }
