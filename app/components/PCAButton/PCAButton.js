0a339ed56b3094e88fbbef464fb26bd4de01ff48
laureano <laureano.pedrojoao@gmail.com>
2018-09-24 17:16:13 -0300

    PCA fixed

--- app/components/PCAButton/PCAButton.js
+++ app/components/PCAButton/PCAButton.js
@@ -14,37 +14,16 @@ export default class PCAButton extends React.Component{
 		let promise = Amostra.getAll();
 		const amostras = await promise
 		//console.log(senha[0]);
-		
-		for(var x in amostras[0])
-			console.log(amostras[0][x]);//Ele consegue capturar todos os atributos de todas as amostras disponíveis 
-			
-		console.log("AA");
+		console.log(amostras[0][2]);
+//		for(var x in amostras[0])
+//			console.log(amostras[0][x]);//Ele consegue capturar todos os atributos de todas as amostras disponíveis 
 	const PCA = require('ml-pca');
-	const dataset = require('ml-dataset-iris').getNumbers();
-	
+	const data = amostras[0].slice(0,2);
+	//console.log(data);
 		// // dataset is a two-dimensional array where rows represent the samples and columns the features
-		const pca = new PCA(dataset);
+		const pca = new PCA(amostras);
+//		console.log(dataset);
 		console.log(pca.getExplainedVariance());
-		 /*
-		// [ 0.9246187232017269,
-		  // 0.05306648311706785,
-		  // 0.017102609807929704,
-		  // 0.005212183873275558 ]
-		// */
-		const newPoints = [[4.9, 3.2, 1.2, 0.4], [5.4, 3.3, 1.4, 0.9]];
-		console.log(pca.predict(newPoints)); // project new points into the PCA space
-		/*
-		// [
-		  // [ -2.830722471866897,
-			// 0.01139060953209596,
-			// 0.0030369648815961603,
-			// -0.2817812120420965 ],
-		  // [ -2.308002707614927,
-			// -0.3175048770719249,
-			// 0.059976053412802766,
-			// -0.688413413360567 ]]
-		// */
-			
 	}
 	
     render() {
