b4745ff129c4820ab2bfe83e06c0c76b777f80b2
laureano <laureano.pedrojoao@gmail.com>
2018-09-24 18:51:29 -0300

    PCA Fixed for real

--- app/components/PCAButton/PCAButton.js
+++ app/components/PCAButton/PCAButton.js
@@ -11,20 +11,29 @@ export default class PCAButton extends React.Component{
 		this.handleClick = this.test.bind(this);
 	}
     test = async () => {
+		
 		let promise = Amostra.getAll();
 		const amostras = await promise
 		//console.log(senha[0]);
 	//	console.log(amostras[0][2]);
-//		for(var x in amostras[0])
-//			console.log(amostras[0][x]);//Ele consegue capturar todos os atributos de todas as amostras disponíveis 
+	
+
+	Object.keys(amostras[0]).forEach(propriedade => {
+		arrayComNumeros.push(amostras[0][propriedade])
+	})
+	listaDelistas = []
+	for(var i = 0 ; i < amostras.length; i++){
+		let arrayComNumeros = []
+		Object.keys(amostras[i]).forEach(propriedade => {
+			arrayComNumeros.push(amostras[i][propriedade])
+		})
+		listaDelistas.push(arrayComNumeros)
+	}	//Ele consegue capturar todos os atributos de todas as amostras disponíveis 
 	const PCA = require('ml-pca');
-	const data = JSON.parse(JSON.stringify(amostras));
-	for(var instance in amostras){
-			console.log(instance.toString);
-	}
-	//console.log(data);
+	//const data = JSON.parse(JSON.stringify(amostras));
+	
 		// // dataset is a two-dimensional array where rows represent the samples and columns the features
-		const pca = new PCA(amostras);
+		const pca = new PCA(listaDelistas);
 //		console.log(dataset);
 		//console.log(pca.getExplainedVariance());
 	}
