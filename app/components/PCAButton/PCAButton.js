083f7f541ee384ce3a087dbccbdf43decd692624
laureano <laureano.pedrojoao@gmail.com>
2018-09-25 16:00:26 -0300

    PCA working

--- app/components/PCAButton/PCAButton.js
+++ app/components/PCAButton/PCAButton.js
@@ -11,33 +11,40 @@ export default class PCAButton extends React.Component{
 		this.handleClick = this.test.bind(this);
 	}
     test = async () => {
-		
-		let promise = Amostra.getAll();
-		const amostras = await promise
-		//console.log(senha[0]);
-	//	console.log(amostras[0][2]);
-	
 
-	Object.keys(amostras[0]).forEach(propriedade => {
-		arrayComNumeros.push(amostras[0][propriedade])
-	})
-	listaDelistas = []
-	for(var i = 0 ; i < amostras.length; i++){
-		let arrayComNumeros = []
-		Object.keys(amostras[i]).forEach(propriedade => {
-			arrayComNumeros.push(amostras[i][propriedade])
-		})
-		listaDelistas.push(arrayComNumeros)
-	}	//Ele consegue capturar todos os atributos de todas as amostras disponíveis 
-	const PCA = require('ml-pca');
-	//const data = JSON.parse(JSON.stringify(amostras));
-	
+		var values = await this.getAllDatas();
+		const PCA = require('ml-pca');
+		
 		// // dataset is a two-dimensional array where rows represent the samples and columns the features
-		const pca = new PCA(listaDelistas);
-//		console.log(dataset);
-		//console.log(pca.getExplainedVariance());
+		const pca = new PCA(values);
+		console.log("<PREDICT>");
+		console.log(pca.predict(values));
+		console.log("</PREDICT>");
+		console.log("<Variances>");
+		console.log(pca.getExplainedVariance(values).slice(0,3));
+		console.log("</Variances>");
+		return { 
+			predict: pca.predict(values),
+			explained_variance: pca.getExplainedVariance(values).slice(0,2)
+		};
 	}
-	
+	getAllDatas = async () => {
+
+		let promise = Amostra.getAllComp();
+		const amostras = await promise;
+		return this.RDPtoArray(amostras);
+
+	}
+	RDPtoArray(arrayOfObjects){ // Pass a RowDataPacket{Standard AMOSTRADAO return datatype} and transform to a ARRAY YAY
+		let arrayOfArraysOfNumbers = arrayOfObjects.map(a => {
+			let result = []
+			Object.keys(a).forEach(key => {
+				result.push(a[key])
+			})
+			return result
+		})
+		return arrayOfArraysOfNumbers;
+	}
     render() {
         return(
             <div>
