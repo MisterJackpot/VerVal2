27a914bb04891507ef8808afe40ad9bdb0aa1095
Felipe <felipe.boff@acad.pucrs.br>
2018-09-25 16:16:15 -0300

    Some comments on PCA

--- app/components/PCAButton/PCAButton.js
+++ app/components/PCAButton/PCAButton.js
@@ -14,17 +14,20 @@ export default class PCAButton extends React.Component{
 
 		var values = await this.getAllDatas();
 		const PCA = require('ml-pca');
-		
-		// // dataset is a two-dimensional array where rows represent the samples and columns the features
+		// pca é instanciado carregando todas as instancias
 		const pca = new PCA(values);
-		console.log("<PREDICT>");
-		console.log(pca.predict(values));
-		console.log("</PREDICT>");
-		console.log("<Variances>");
-		console.log(pca.getExplainedVariance(values).slice(0,3));
-		console.log("</Variances>");
+		//console.log("<PREDICT>");
+		//console.log(pca.predict(values));
+		//console.log("</PREDICT>");
+		//console.log("<Variances>");
+		//console.log(pca.getExplainedVariance(values).slice(0,3));
+		//console.log("</Variances>");
 		return { 
+		    // predict: dimensoes (pc1,pc2,pc3) para cada instancia
 			predict: pca.predict(values),
+			// explained_variance: valores de cada dimensao (pc1,pc2,pc3)
+			// A soma do explained_variance é igual a porcentagem total de quão "similar" o modelo atual é do dataset original
+			// Quanto maior for a soma (maximo igual a 1) do explained_variance, mais garantido é a resiliência do modelo gerado
 			explained_variance: pca.getExplainedVariance(values).slice(0,2)
 		};
 	}
