06b022e1c0768edd7ad5acba183e751162c90ed2
guilherme.piccoli <guilherme.piccoli@acad.pucrs.br>
2018-10-10 19:00:02 -0300

    select charts

--- app/components/ChangeGraphComponent/ChangeGraphComponent.js
+++ app/components/ChangeGraphComponent/ChangeGraphComponent.js
@@ -10,7 +10,9 @@ export default class ChangeGraphComponent extends React.Component{
         
         const options = [
             {value: '0', label: '3D'},
-            {value: '1', label: 'P1-P2'}
+            {value: '1', label: 'P1-P2'},
+            {value: '2', label: 'P2-P3'},
+            {value: '3', label: 'P1-P3'}
         ]
         const customStyles = {
             control: (base, state) => ({
--- app/containers/MyEchartContainer/MyEchartContainer.js
+++ app/containers/MyEchartContainer/MyEchartContainer.js
@@ -4,7 +4,7 @@ import Chart2D from '../../components/MyCharts/Chart2D';
 import NavigateButtonComponent from '../../components/NavigateButtonComponent/NavigateButtonComponent';
 import ChangeGraphComponent from '../../components/ChangeGraphComponent/ChangeGraphComponent';
 import { Redirect } from 'react-router';
-import PCA from '../../utils/BO/PCABO.js'
+import PCA from '../../utils/BO/PCABO.js';
 
 type Props = {};
 
@@ -12,7 +12,9 @@ export default class MyChartContainer extends React.Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
-      typeChart: true,
+      is3D: false,
+      horizontal: '',
+      vertical: '',
       csv: false,
       items: []
     };
@@ -24,34 +26,46 @@ export default class MyChartContainer extends React.Component<Props> {
         amostras.push(element);
       });
       this.setState({ items: amostras });
-      this.setState({ typeChart: !this.state.typeChart });
-      
+      this.setState({ is3D: !this.state.is3D });
     });
   }
 
-  onClick = () => {
-    this.setState({ typeChart: !this.state.typeChart });
+  onChangeCheckbox = e => {
+    if (e.value == '0') {
+      this.setState({ is3D: true });
+    } else if (e.value == '1') {
+      this.setState({ is3D: false, horizontal: 'P1', vertical: 'P2' });
+    } else if (e.value == '2') {
+      this.setState({ is3D: false, horizontal: 'P2', vertical: 'P3' });
+    } else if (e.value == '3') {
+      this.setState({ is3D: false, horizontal: 'P1', vertical: 'P3' });
+    }
   };
 
-  onChangeCheckbox = (e) => {
-    console.log(e.value);
-    if(e.value == '0'){
-      this.setState({typeChart: false});
-    }
-    else if(e.value == '1'){
-      this.setState({typeChart: true});
+  teste() {
+    if (this.state.is3D) {
+    return (<Chart3D list={this.state.items} />);
+    } else {
+      return (
+        <Chart2D
+          list={this.state.items}
+          horizontal='P2'
+          vertical='P3'
+        />
+      );
     }
   }
 
-
   render() {
-    const typeChart = this.state.typeChart;
+    const is3D = this.state.is3D;
+    const horizontal = this.state.horizontal;
+    const vertical = this.state.vertical;
     return (
       <div style={{ width: '100%', height: '100%' }}>
-        <div align="center" style={{paddingTop:'2.8rem'}}>
-          <ChangeGraphComponent change={this.onChangeCheckbox}></ChangeGraphComponent>
+        <div align="center" style={{ paddingTop: '2.8rem' }}>
+          <ChangeGraphComponent change={this.onChangeCheckbox} />
         </div>
-        {typeChart ? <Chart2D list={this.state.items} /> : <Chart3D list={this.state.items} />}
+        {this.teste()}
       </div>
     );
   }
