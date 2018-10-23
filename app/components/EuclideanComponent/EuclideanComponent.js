76fcd5f7cfdd636938a4cbf3938c2dc384bcdca2
laureano <laureano.pedrojoao@gmail.com>
2018-10-23 16:48:02 -0300

    All distance Euclidean

--- app/components/EuclideanComponent/EuclideanComponent.js
+++ app/components/EuclideanComponent/EuclideanComponent.js
@@ -7,10 +7,7 @@ type Props = {};
 export default class EuclideanComponent extends React.Component{
     props: Props;
     async doEuclidean(){
-
-        console.log(Euclides.getDistanceUsingID("082.1","082.1"));
-        console.log(Euclides.getDistanceUsingID("082.1","085.2"));
-        console.log(Euclides.getDistanceUsingID("082.1","086.1"));
+        console.log(Euclides.getAllCorrelation("082.2"));
     }
     
     
--- app/utils/BO/EuclideanBO.js
+++ app/utils/BO/EuclideanBO.js
@@ -7,7 +7,7 @@ import Amostras from "../DB/DAO/AmostraDAO";
         },
          getDistanceUsingID: async(id1,id2) =>{
             let amostra1 = (await Amostras.getBytId(id1));
-            let amostra2 = (await Amostras.getBytId(id2));
+			let amostra2 = (await Amostras.getBytId(id2));
 			let amostra1Arr = amostra1.map(a => {
 				let result = []
 				Object.keys(a).forEach(key => {
@@ -25,6 +25,29 @@ import Amostras from "../DB/DAO/AmostraDAO";
 			})
             const { euclidean, squaredEuclidean } = require('ml-distance-euclidean');
             console.log(euclidean(amostra1Arr[0],amostra2Arr[0]));
-        }
+		},
+		getAllCorrelation: async(amostraEscolhida) =>{
+            const { euclidean, squaredEuclidean } = require('ml-distance-euclidean');
+			let allAmostrasBut = await Amostras.getAllButComp(amostraEscolhida);
+			let amostra = await Amostras.getBytId(amostraEscolhida);
+			allAmostrasBut = allAmostrasBut.map(a => {
+				let result = []
+				Object.keys(a).forEach(key => {
+					result.push(a[key])
+				})
+				return result
+			})
+			amostra = amostra.map(a => {
+				let result = []
+				Object.keys(a).forEach(key => {
+					result.push(a[key])
+				})
+				return result
+			})
+			let allDistance = Array();
+			for(let i = 0; i < allAmostrasBut.length;i++)
+				allDistance[i] = euclidean(amostra[0],allAmostrasBut[i]);
+			console.log(allDistance);
+		}
 	}
 export default Euclidean;
