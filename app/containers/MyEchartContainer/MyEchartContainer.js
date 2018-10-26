2deb4290317c57deb64e69424221c82c304667c7
PedroHenrique98 <pedro.medeiros.001@acad.pucrs.br>
2018-10-25 21:05:29 -0300

    divisao de componente 2D

--- app/containers/MyEchartContainer/MyEchartContainer.js
+++ app/containers/MyEchartContainer/MyEchartContainer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Chart3D from '../../components/MyCharts/Chart3D';
-import Chart2D from '../../components/MyCharts/Chart2D';
-import NavigateButtonComponent from '../../components/NavigateButtonComponent/NavigateButtonComponent';
+import Chart2Dp1p2 from '../../components/MyCharts/Chart2Dp1p2';
+import Chart2Dp1p3 from '../../components/MyCharts/Chart2Dp1p3';
+import Chart2Dp2p3 from '../../components/MyCharts/Chart2Dp2p3';
 import ChangeGraphComponent from '../../components/ChangeGraphComponent/ChangeGraphComponent';
-import { Redirect } from 'react-router';
 import PCA from '../../utils/BO/PCABO.js';
 
 type Props = {};
@@ -20,9 +20,8 @@ export default class MyChartContainer extends React.Component<Props> {
     };
 
     PCA.getPCA().then(result => {
-      var amostras = [];
-      var array = result;
-      array.forEach(element => {
+      const amostras = [];
+      result.forEach(element => {
         amostras.push(element);
       });
       this.setState({ items: amostras });
@@ -33,43 +32,47 @@ export default class MyChartContainer extends React.Component<Props> {
   componentWillMount(){
     this.forceUpdate()
   }
+
   onChangeCheckbox = e => {
-    if (e.value == '0') {
+    if (e.value === '0') {
       this.setState({ typeChart: 1 });
-    } else if (e.value == '1') {
+    }
+    if (e.value === '1') {
       this.setState({ typeChart: 2});
-    } else if (e.value == '2') {
+    }
+    if (e.value === '2') {
       this.setState({ typeChart: 3});
-    } else if (e.value == '3') {
+    }
+    if (e.value === '3') {
       this.setState({ typeChart: 4});
     }
   };
 
   myChart() {
-    if (this.state.typeChart == 1) {
+    if (this.state.typeChart === 1) {
     return (<Chart3D list={this.state.items} />);
     }
-    if (this.state.typeChart == 2) {
+    if (this.state.typeChart === 2) {
       return (
-        <Chart2D
+        <Chart2Dp1p2
           list={this.state.items}
           horizontal='P1'
           vertical='P2'
         />
       );
     }
-    if (this.state.typeChart == 3) {
+    if (this.state.typeChart === 3) {
       return (
-        <Chart2D
+        <Chart2Dp2p3
           list={this.state.items}
           horizontal='P2'
           vertical='P3'
         />
       );
     }
-     if (this.state.typeChart == 4) {
+     if (this.state.typeChart === 4) {
       return (
-        <Chart2D
+        <Chart2Dp1p3
           list={this.state.items}
           horizontal='P1'
           vertical='P3'
@@ -79,9 +82,6 @@ export default class MyChartContainer extends React.Component<Props> {
   }
 
   render() {
-    const typeChart = this.state.typeChart;
-    const horizontal = this.state.horizontal;
-    const vertical = this.state.vertical;
     return (
       <div style={{ width: '100%', height: '100%' }}>
         <div align="center" style={{ paddingTop: '2.8rem' }}>
