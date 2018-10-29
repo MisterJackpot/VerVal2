32b6f6ff44b860ca815d4ce2f73a0fbd1aa721ab
Eduardo Lessa <eduardo.lessa@acad.pucrs.br>
2018-10-29 19:46:39 -0200

    Pdf com erro

--- app/containers/Counter/CounterContainer.js
+++ app/containers/Counter/CounterContainer.js
@@ -9,6 +9,13 @@ import AmostraBO from '../../utils/BO/AmostraBO';
 import Alert from 'react-s-alert';
 import styles from './Modal.css';
 
+const electron = require('electron');
+const fs = require('fs');
+const os = require('os');
+const BrowserWindow = electron.remote.BrowserWindow;
+const ipc = require('electron').ipcRenderer;
+const shell = electron.remote.shell;
+
 type Props = {};
 
 export default class CounterPage extends Component<Props> {
@@ -16,6 +23,26 @@ export default class CounterPage extends Component<Props> {
   props: Props;
 
   state = { show: false }
+
+  gerarPDF = () => {
+    ipc.on('print-to-pdf' => {
+      console.log('primeiro ipc');
+      const pdfPath = ('./meuPdf.pdf');
+      const win = BrowserWindow.fromWebContents(event.sender);
+
+      win.webContents.printToPDF({}, (error, data) => {
+        if(error) return console.log(error.message);
+      
+        fs.writeFile(pdfPath, data, err => {
+          if(err) return console.log(err.message);
+        })
+      })
+    })
+  }
+
+  callPDF = (event) => {
+    ipc.send('print-to-pdf', event)
+  }
 
   showModal = () => {
     this.setState({ show: true });
@@ -46,7 +73,7 @@ export default class CounterPage extends Component<Props> {
               effect: 'stackslide',
               timeout: 10000
             });
-          } else {
+          } else {ipc.on
             Alert.error('Amostras repetidas, log salvo em ' + err, {
               position: 'top',
               effect: 'stackslide',
@@ -69,6 +96,7 @@ export default class CounterPage extends Component<Props> {
       <div className={styles.botaoAmostras}>
         <button type='button' className={styles.showmodal + ' ' + styles.grande} onClick={this.showModal}>Adicionar Amostras</button>
         <button type='button' className={styles.showmodal + ' ' + styles.pequeno} onClick={this.showModal}>+</button>
+        <button type='button' onClick={this.callPDF}>Gerar PDF</button>
         <div>
           <Modal show={this.state.show} handleClose={this.hideModal}>
             <UploadCsv acceptedFunction={this.insereAmostras}/>
