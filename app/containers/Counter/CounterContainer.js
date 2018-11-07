0e3a97ef5ad04c0aca08f9f5bec05ef299092243
gabriel.lima <gabriel.lima.006@acad.pucrs.br>
2018-11-07 01:18:29 -0200

    estilizando botão PDF

--- app/containers/Counter/CounterContainer.js
+++ app/containers/Counter/CounterContainer.js
@@ -96,7 +96,12 @@ export default class CounterPage extends Component<Props> {
       <div className={styles.botaoAmostras}>
         <button type='button' className={styles.showmodal + ' ' + styles.grande} onClick={this.showModal}>Adicionar Amostras</button>
         <button type='button' className={styles.showmodal + ' ' + styles.pequeno} onClick={this.showModal}>+</button>
-        <button type='button' onClick={this.gerarPDF}>Gerar PDF</button>
+        <button type='button' className={styles.showmodal + ' ' + styles.grande + ' ' + styles.pdf} onClick={this.gerarPDF}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="14" viewBox="0 0 1rem 1rem"><path stroke="none" fill="none" d="M0 0h24v24H0z"/><path stroke="none" fill="#ffffff" d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8.5 7.5c0 .83-.67 1.5-1.5 1.5H9v2H7.5V7H10c.83 0 1.5.67 1.5 1.5v1zm5 2c0 .83-.67 1.5-1.5 1.5h-2.5V7H15c.83 0 1.5.67 1.5 1.5v3zm4-3H19v1h1.5V11H19v2h-1.5V7h3v1.5zM9 9.5h1v-1H9v1zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm10 5.5h1v-3h-1v3z"/></svg>
+        </button>
+        <button type='button' className={styles.showmodal + ' ' + styles.pequeno + ' ' + styles.pdf} onClick={this.gerarPDF}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 1.5rem 1.5rem"><path stroke="none" fill="none" d="M0 0h24v24H0z"/><path stroke="none" fill="#ffffff" d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8.5 7.5c0 .83-.67 1.5-1.5 1.5H9v2H7.5V7H10c.83 0 1.5.67 1.5 1.5v1zm5 2c0 .83-.67 1.5-1.5 1.5h-2.5V7H15c.83 0 1.5.67 1.5 1.5v3zm4-3H19v1h1.5V11H19v2h-1.5V7h3v1.5zM9 9.5h1v-1H9v1zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm10 5.5h1v-3h-1v3z"/></svg>
+        </button>
         <div>
           <Modal show={this.state.show} handleClose={this.hideModal}>
             <UploadCsv acceptedFunction={this.insereAmostras}/>
