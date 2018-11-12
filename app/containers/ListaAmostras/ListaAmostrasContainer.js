de9b8d749566f024af40d15c4b298c70e5328a26
gabriel.lima <gabriel.lima.006@acad.pucrs.br>
2018-11-12 18:15:56 -0200

    estilizando componentes

--- app/containers/ListaAmostras/ListaAmostrasContainer.js
+++ app/containers/ListaAmostras/ListaAmostrasContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FilteredList from '../../components/ListaAmostrasComponent/ReactFilterListComponent';
 import CounterContainer from '../Counter/CounterContainer.js';
+import styles from './ListaAmostrasContainer.css'
 
 type Props = {};
 
@@ -8,7 +9,7 @@ export default class ListaAmostrasContainer extends Component<Props> {
 
   render() {
     return (
-      <div>
+      <div className={styles.listcontainer}>
         <FilteredList setAmostraSelecionada={this.props.setAmostraSelecionada}/>
         <CounterContainer/>
       </div>
