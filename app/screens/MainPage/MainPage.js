dc618b36b94b36a36ddb528459e19c97712cb3a3
guilherme.piccoli <guilherme.piccoli@acad.pucrs.br>
2018-11-29 17:47:26 -0200

    filtro de data removido, esta na branch filtroData para correcoes futuras

--- app/screens/MainPage/MainPage.js
+++ app/screens/MainPage/MainPage.js
@@ -1,108 +1,47 @@
-// @flow
-import React, { Component } from 'react';
-import CounterContainer from '../../containers/Counter/CounterContainer';
-import Chart3D from '../../components/MyCharts/Chart3D';
-// import Chart2D from '../../components/MyCharts/';
-import DatePicker from "react-datepicker";
-import Chart2Dp1p2 from '../../components/MyCharts/Chart2Dp1p2';
+import React from 'react'
 import MyChartContainer from '../../containers/MyEchartContainer/MyEchartContainer'
 import ListContainer from '../../containers/ListaAmostras/ListaAmostrasContainer'
-import ListDataContainer from '../../containers/ListDataContainer/ListDataContainer'
 import styles from './MainPage.css'
 import ListCorrelationContainer from '../../containers/ListCorrelationContainer/ListCorrelationContainer'
-import moment from 'moment'
-import Filter from "../../utils/BO/FilterBO"
 type Props = {};
-import "react-datepicker/dist/react-datepicker.css";
 
-export default class MainPage extends Component<Props> {
+export default class MainPage extends React.Component<Props> {
   props: Props;
 
   constructor(props) {
       super(props);
       this.state = {
-        render:false,
-        renderByCorrelation: false,
-        renderByData: false,
-        amostraSelecionada: '',
-        startDateInicio: moment(),
-        startDateFinal: moment()
-
+        renderCorrelation: false,
+        amostraSelecionada: ''
     }
   }
 
   setAmostraSelecionada = (amostra) => {
     this.setState( () => ({amostraSelecionada: amostra}) )
-    this.filtrarCorrelacao();
+    this.setRenderCorrelaction()
   }
 
-  setRender = () => {
+  setRenderCorrelaction = () => {
       this.setState(prevState => ({
-        render: !prevState.render}))
-        if(this.state.renderByCorrelation)
-        console.log("Estou usando a correlação!")
-        else if(this.state.renderByData)
-        console.log("Estou usando a data!");
+          renderCorrelation: !prevState.renderCorrelation}))
   }
 
-  selecionaDataInicio = (dateInicio) => {
-    this.setState({
-      startDateInicio: dateInicio
-    });
-  }
-
-  selecionaDataFinal = (dateFinal) => {
-    this.setState({
-      startDateFinal: dateFinal
-    });
-  }
-  unsetRender = () =>{
-    this.setState({
-      render: false
-    });
-    if(this.state.renderByCorrelation)
-    this.setState(prevState => ({ renderByCorrelation: false}));
-    else 
-    this.setState(prevState => ({ renderByData: false}));
-    
-  }
-  filtrarAmostras = async () => {
-    this.setState(prevState => ({ renderByData: !prevState.renderByData}));
-    this.setRender();
-  }
-  filtrarCorrelacao = async () => { 
-    this.setState(prevState => ({ renderByCorrelation: !prevState.renderByCorrelation}));  
-    this.setRender();
-  }
-
-  render() {
-    let containerRender;
-    if(this.state.render)
-      if(this.state.renderByData)
-       containerRender = <ListDataContainer dataInicio={this.state.startDateInicio} dataFinal = {this.state.startDateFinal}/>
-      else
-      containerRender = <ListCorrelationContainer amostra={this.state.amostraSelecionada} />
+  render() {    
     return (
       <div className={styles.container}>
-            <div className={styles.leftContainer}>            
-              <MyChartContainer/>
-              <div className={styles.datePickerDiv}>            
-                <DatePicker className={styles.datePicker} dateFormat="DD/MM/YYYY"  selected={this.state.startDateInicio}  onChange={this.selecionaDataInicio} />
-                <DatePicker className={styles.datePicker} dateFormat="DD/MM/YYYY"  selected={this.state.startDateFinal}  onChange={this.selecionaDataFinal} />
-                <button className={styles.button} onClick={!this.state.renderByData ? this.filtrarAmostras:null}>Filtrar</button>
-              </div>
+            <div className={styles.leftContainer}>
+                <MyChartContainer/>
             </div>
-            <div className={styles.rightContainer}>
-            {this.state.render ? (
+            <div className={styles.rightContainer}>{this.state.renderCorrelation ? (
               <div>
-                <button className={styles.button} onClick={this.unsetRender}>
+                <button className={styles.button} onClick={this.setRenderCorrelaction}>
                     Voltar
                 </button>
-                {containerRender}
+                <ListCorrelationContainer amostra={this.state.amostraSelecionada}/>
               </div>
-            ) : (<ListContainer setAmostraSelecionada={this.setAmostraSelecionada}/>)}
+            ) : (<ListContainer setAmostraSelecionada={this.setAmostraSelecionada} />)}
             </div>
-
       </div>
+
   )}
 }
